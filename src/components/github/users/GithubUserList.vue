b29c55625ac0a5d97d45b34c34be389531318189
igrdi <id5346216@gmail.com>
2022-06-03 12:47:20 +0300

    Base functionlity: Display  base user information

--- src/components/github/users/GithubUserList.vue
+++ src/components/github/users/GithubUserList.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="github-user-list-component">
-    github-user-list-component Works
+    <DataView :value="users">
+			<template #header>
+        <InputText v-model="searchTerm" placeholder="Search GitHub" />
+			</template>
+
+			<template #list="{ data }">
+        <div class="w-full flex">
+          <div>
+            <Avatar :image="data.avatarUrl"  class="mr-2" shape="circle" />
+          </div>
+          <div>
+            <div>{{data.name}}</div>
+            <div>{{data.login}}</div>
+            <div>{{data.bio}}</div>
+            <div>{{data.location}}</div>
+          </div>
+				</div>    
+			</template>
+		</DataView>
   </div>
 </template>
+
+
+<script lang="ts">
+import { GithubSearchUserModel } from '@/services/github';
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  props: {
+    users: Array as PropType<GithubSearchUserModel[]>
+  },
+  data(): {
+    searchTerm?: string
+  } {
+    return {
+      searchTerm: undefined,
+    }
+  }
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,18 @@ import PrimeVue from 'primevue/config';
 import axios from 'axios';
 import { axiosConfigure } from './config';
 import { addAppProviders, APP_PROVIDERS } from './dependency-injection';
+import DataView from 'primevue/dataview';
+import InputText from 'primevue/inputtext';
+import Avatar from 'primevue/avatar';
 
 const app = createApp(App);
 app.use(router);
 
 app.use(PrimeVue);
+app.component('DataView', DataView);
+app.component('InputText', InputText);
+app.component('Avatar', Avatar);
+
 axiosConfigure(axios);
 addAppProviders(app, APP_PROVIDERS);
 
